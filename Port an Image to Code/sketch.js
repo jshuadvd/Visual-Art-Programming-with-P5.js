e8277694e208a2821a2331cb1451ff84e14d3e8f
jshuadvd <jshuadvd@icloud.com>
2016-03-05 23:10:59 -0800

    initial Commit

---
+++ Port an Image to Code/sketch.js
@@ -0,0 +1,14 @@
+/**
+ * Assignment 1: Port an Image to Code
+ * By: Joshua David AKA jshuadvd
+ */
+
+
+function setup() {
+  createCanvas(1024, 768);
+  background(0);
+}
+
+function draw() {
+
+}

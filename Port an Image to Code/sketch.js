4ee7e8b6a0b6041d89c00d3eb9a5ae0c500bbe4e
jshuadvd <jshuadvd@icloud.com>
2016-03-06 00:39:07 -0800

    Added smoothness

--- Port an Image to Code/sketch.js
+++ Port an Image to Code/sketch.js
@@ -7,6 +7,8 @@
 function setup() {
   createCanvas(1024, 768);
   background(0);
+  smooth();
+
 }
 
 function draw() {
@@ -16,11 +18,21 @@ function draw() {
   rect(0, 0, 24, 768);
 
   // Light Green Square
-  fill(130, 166, 131, 100);
+  fill(130, 166, 131, 50);
   beginShape();
     vertex(24, 0);
     vertex(24, 260);
     vertex(336, 280);
     vertex(350, 0)
+  endShape();
+
+  // Next Light Green Square
+  fill(130, 168, 148, 10);
+  tint(255, 120);
+  beginShape();
+    vertex(348, 0);
+    vertex(340, 228);
+    vertex(675, 250);
+    vertex(700, 0)
   endShape();
 }

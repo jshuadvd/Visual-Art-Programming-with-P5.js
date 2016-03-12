8aa0732188f94cfeb3cb3f0c61cb74ce772f4fa1
jshuadvd <jshuadvd@icloud.com>
2016-03-12 01:26:51 -0800

    Corrected colors array

--- Triangles/sketch.js
+++ Triangles/sketch.js
@@ -1,28 +1,28 @@
-var colors = color('#ff0329',
-                   '#d13551',
-                   '#dd4655',
-                   '#EB516B',
-                   '#FF6072',
-                   '#00526c',
-                   '#10828c',
-                   '#009ba8',
-                   '#03a4ae',
-                   '#04b8b7',
-                   '#00b0b2',
-                   '#00b7a9',
-                   '#00b392',
-                   '#00bb86',
-                   '#d7b74a',
-                   '#dece67',
-                   '#d9d26a',
-                   '#fff22d',
-                   '#dec358',
-                   '#e7a942',
-                   '#da7837',
-                   '#e6683c',
-                   '#ff7454
-                   '#e6894e'
-                 );
+var colors = ['#ff0329',
+              '#d13551',
+              '#dd4655',
+              '#EB516B',
+              '#FF6072',
+              '#00526c',
+              '#10828c',
+              '#009ba8',
+              '#03a4ae',
+              '#04b8b7',
+              '#00b0b2',
+              '#00b7a9',
+              '#00b392',
+              '#00bb86',
+              '#d7b74a',
+              '#dece67',
+              '#d9d26a',
+              '#fff22d',
+              '#dec358',
+              '#e7a942',
+              '#da7837',
+              '#e6683c',
+              '#ff7454',
+              '#e6894e'
+             ];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -42,6 +42,7 @@ function draw() {
 }
 
 function drawTriangles() {
+  // fill(colors);
   // fill(209, 17, 28);
   noStroke();
   triangle(0, 48, 28, 0, 54, 48);

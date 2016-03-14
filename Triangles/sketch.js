85a8b4001283474b901cd2624e1d38ff01e26d11
jshuadvd <jshuadvd@icloud.com>
2016-03-14 12:05:59 -0700

    Removed unused code

--- Triangles/sketch.js
+++ Triangles/sketch.js
@@ -43,18 +43,14 @@ function draw() {
 
 function drawTriangles() {
 
-  // for (var i = 0; i < colors.length; i++) {
-    // console.log(colors[i]);
-    // fill(colors[0]);
-    // fill(colors[Math.floor(random(colors.length))]);
+    fill(colors[4]);
+    //fill(colors[Math.floor(random(colors.length))]);
     // fill(209, 17, 28);
-
     noStroke();
     triangle(0, 48, 28, 0, 54, 48);
     //fill(colors[Math.floor(random(colors.length))]);
-    //fill(colors[i]);
+    fill(colors[7]);
     // fill(0, 174, 219);
-    fill(c2);
+    //fill(c2);
     triangle(28, 0, 54, 48, 82, 0);
-  // }
 }

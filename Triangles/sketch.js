519035365c17e1777f1bdd6f7f2b8338d6106e60
jshuadvd <jshuadvd@icloud.com>
2016-03-14 12:30:26 -0700

    Creating art now

--- Triangles/sketch.js
+++ Triangles/sketch.js
@@ -1,31 +1,32 @@
-var colors = ['#ff0329',
-              '#d13551',
-              '#dd4655',
-              '#EB516B',
-              '#FF6072',
-              '#00526c',
-              '#10828c',
-              '#009ba8',
-              '#03a4ae',
-              '#04b8b7',
-              '#00b0b2',
-              '#00b7a9',
-              '#00b392',
-              '#00bb86',
-              '#d7b74a',
-              '#dece67',
-              '#d9d26a',
-              '#fff22d',
-              '#dec358',
-              '#e7a942',
-              '#da7837',
-              '#e6683c',
-              '#ff7454',
-              '#e6894e'
+var colors = ['#ff0329', // 0
+              '#d13551', // 1
+              '#dd4655', // 2
+              '#EB516B', // 3
+              '#FF6072', // 4
+              '#00526c', // 5
+              '#10828c', // 6
+              '#009ba8', // 7
+              '#03a4ae', // 8
+              '#04b8b7', // 9
+              '#00b0b2', // 10
+              '#00b7a9', // 11
+              '#00b392', // 12
+              '#00bb86', // 13
+              '#d7b74a', // 14
+              '#dece67', // 15
+              '#d9d26a', // 16
+              '#fff22d', // 17
+              '#dec358', // 18
+              '#e7a942', // 19
+              '#da7837', // 20
+              '#e6683c', // 21
+              '#ff7454', // 22
+              '#e6894e'  // 23
              ];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noLoop();
 }
 
 function draw() {
@@ -43,16 +44,22 @@ function draw() {
 
 function drawTriangles() {
 
-    fill(colors[5]);
+    // fill(colors[5]);
     // fill(colors[Math.floor(random(colors.length))]);
     // fill(209, 17, 28);
+    fill(colors[5]);
     noStroke();
     triangle(random(255), random(255), random(255), random(255), random(255), random(255));
     // triangle(0, 48, 28, 0, 54, 48);
     // fill(colors[Math.floor(random(colors.length))]);
     fill(colors[7]);
+    triangle(random(255), random(255), random(255), random(255), random(255), random(255));
     // fill(0, 174, 219);
     // triangle(28, 0, 54, 48, 82, 0);
+
+    fill(colors[13]);
+    // stroke(255, 96, 114);
+    // strokeWeight(4);
     triangle(random(255), random(255), random(255), random(255), random(255), random(255));
 
 }

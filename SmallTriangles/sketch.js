490b8f7b89f35b62c7d7277093827dd181ca36e9
Joshua David <jshuadvd@icloud.com>
2016-05-02 23:12:28 -0700

    Update sketch.js

--- SmallTriangles/sketch.js
+++ SmallTriangles/sketch.js
@@ -1,6 +1,9 @@
-/**
+/*
+ **
  * By: Joshua David AKA jshuadvd
- **/
+ **
+ */
+ 
  
 /* Still need to determine how to randomly 
 (And in color sequence, distribute colors in a good scale pattern) */
